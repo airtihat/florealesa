f48d0f3e0bb13f1dc70e9daf3ad958e69a0cbcae
airtihat <airtihat@gmail.com>
2025-06-19 18:20:01 +0300

    2

--- florealesa/urls.py
+++ florealesa/urls.py
@@ -1,19 +1,16 @@
-# florealesa/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from storefront.views import home_view  # يمكنك حذف هذا إذا كانت الصفحة الرئيسية داخل storefront.urls
+from storefront.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view, name='home'),  # أو استخدم include حسب طريقة تنظيمك
+    path('', home_view, name='home'),
     path('core/', include('core.urls')),
     path('storefront/', include('storefront.urls')),
     path('orders/', include('orders.urls')),
 ]
 
-# دعم ملفات الصور أثناء التطوير
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
